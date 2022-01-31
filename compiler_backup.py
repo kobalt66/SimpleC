4d0906456855d4d06f1911a5313b91f6626d3850
LePhobix_ <40542349+kobalt66@users.noreply.github.com>
2022-01-31 13:41:57 +0100

    Update compiler_backup.py

--- compiler_backup.py
+++ compiler_backup.py
@@ -12,6 +12,7 @@
 # I hope you enjoy my language :)                                                            #
 ##############################################################################################
 
+from re import L
 import string
 import os
 import numpy
@@ -23,18 +24,19 @@ import random
 #############################
 
 # Variable types
-VAR = 'var'   # [variable]
-BYT = 'byt'   # [byte]
-CHR = 'chr'   # [character]
-STR = 'str'   # [string]
-INT = 'int'   # [integer]
-FLT = 'flt'   # [floating point]
-DBL = 'dbl'   # [double]
-BOL = 'bol'   # [boolean]
-TYP = 'typ'   # [type]
-LIST = 'list' # [array]
+VAR = 'var' # [variable]
+BYT = 'byt' # [byte]
+CHR = 'chr' # [character]
+STR = 'str' # [string]
+INT = 'int' # [integer]
+FLT = 'flt' # [floating point]
+DBL = 'dbl' # [double]
+BOL = 'bol' # [boolean]
+TYP = 'typ' # [type]
+LST = 'lst' # [array]
 
 VARTYPE = 'VARIABLE'
+TYPEOF = 'typeof'
 VARTYPES = [
     VAR,
     BYT,
@@ -45,8 +47,21 @@ VARTYPES = [
     DBL,
     BOL,
     TYP,
-    LIST
+    LST
 ]
+TYPEOFs = [
+    'VAR',
+    'BYT',
+    'CHR',
+    'STR',
+    'INT',
+    'FLT',
+    'DBL',
+    'BOL',
+    'TYP',
+    'LST'
+]
+
 
 # Keywords
 IF = 'if'
@@ -72,7 +87,6 @@ RETURN = 'return'
 CONTINUE = 'continue'
 BREAK = 'break'
 SIZEOF = 'sizeof'
-TYPEOF = 'typeof'
 LENGHTOF = 'lenghtof'
 NULL = 'null'
 FALSE = 'false'
@@ -105,7 +119,6 @@ KEYWORDS = [
     CONTINUE,
     BREAK,
     SIZEOF,
-    TYPEOF,
     LENGHTOF,
     USING,
     NAMESPACE
@@ -188,8 +201,9 @@ LETGITS = LETTERS + DIGITS
 FLOATMIN = 0.0000000000000001
 
 outputdir = ""
-projectdir = ""
+librarydir = "Libraries"
 fileType = ".sc"
+usedLibs = []
 
 # Extra types
 BON = 'binopnode'
@@ -209,7 +223,7 @@ FUNCTYPES = [
     DBL,
     BOL,
     TYP,
-    LIST
+    LST
 ]
 
 
@@ -704,6 +718,14 @@ class Using:
 
     def __repr__(self):
         return f'\n\tUsing Node : \n\t\tNamespace > {self.name}'
+
+
+class TypeOf:
+    def __init__(self, type):
+        self.type
+        
+    def getType(self):
+        return TYPEOF
 
 
 class Variable:
@@ -1260,6 +1282,8 @@ class Parser:
                     lib = metaNode.value
                 elif metaNode.type == IMPORT:
                     imports.append(metaNode)
+                    if not metaNode.value in usedLibs:
+                        usedLibs.append(metaNode.value)
                 else:
                     metacode.append(metaNode)
             elif self.currTok.type == KEYWORD:
@@ -2827,7 +2851,7 @@ class Parser:
                             "Expected ';'.", SYNTAXERROR,
                             self.currTok.start, self.scriptName))
 
-                return res.success(ReasignVar(left, EQUALS, value))
+                return res.success(ReasignVar(left, Token(EQUALS, EQUALS), value))
             else:
                 self.reverse()
 
@@ -2862,7 +2886,7 @@ class Parser:
         res.registerAdvance()
         self.advance()
 
-        if type == LIST:
+        if type == LST:
             if not self.currTok.type == LSBRACKET:
                 return res.failure(
                     Error(
@@ -2943,7 +2967,7 @@ class Parser:
     def listElements(self):
         res = ParseResult()
 
-        listType = LIST
+        listType = LST
         if self.currTok.type == VARTYPE:
             listType = self.currTok.value
             res.registerAdvance()
@@ -3055,6 +3079,10 @@ class Parser:
             res.registerAdvance()
             self.advance()
             return res.success(Type(tok.type, tok))
+        elif tok.type == TYPEOF:
+            res.registerAdvance()
+            self.advance()
+            return res.success(TypeOf(tok))
         elif tok.type == IDENTIFIER or tok.value in PREDEFINED:
             res.registerAdvance()
             self.advance()
@@ -3263,21 +3291,18 @@ class Parser:
         return res.success(left)
 
 
-def getUsedScripts(root):
-    global projectdir, fileType
+def ParseScripts(projectdir):
     files = os.listdir(projectdir)
     print(files)
     
     for file in files:
         name, extension = os.path.splitext(file)
-        if not extension == fileType or name == root:
+        if not extension == fileType:
             continue
         
-        script, error = openFile(name + fileType)
+        script, error = openFile(projectdir, name)
         if error:
             return error
-        if not '#lib' in script:
-            continue
     
         lexer = Lexer(name, script)
         tokens, error = lexer.genTokens()
@@ -3292,6 +3317,33 @@ def getUsedScripts(root):
 
     return None
 
+def ParseExternLibraries():
+    files = os.listdir(librarydir)
+    
+    for file in files:
+        name, extension = os.path.splitext(file)
+        if not extension == fileType:
+            continue
+        
+        if not name in usedLibs:
+            continue
+        
+        script, error = openFile(librarydir, name)
+        if error:
+            return error
+    
+        lexer = Lexer(name, script)
+        tokens, error = lexer.genTokens()
+        if error:
+            return error
+
+        # Parsing
+        parser = Parser(name, tokens)
+        ast = parser.parse()
+        if ast.error:
+            return ast.error
+
+    return None
 
 ####################
 # - The compiler - #
@@ -4170,6 +4222,8 @@ class compile2Csharp:
             return 'Type'
         elif type == VAR:
             return 'object'
+        elif type == LST:
+            return 'Array'
         elif type == VOID:
             return 'void'
         elif isinstance(type, str):
@@ -4191,6 +4245,9 @@ class compile2Csharp:
             if isinstance(part.name, str):
                 self.write(
                     f'\n{self.genReasign(part.name, part.op, part.value)};')
+            elif isinstance(part.name, ListAccess):
+                 self.write(
+                    f'\n{self.genReasign(self.genOperationPart(part.name), part.op, part.value)};')
             else:
                 self.write(
                     f'\n{self.genReasign(part.name.varName, part.op, part.value)};')
@@ -4555,9 +4612,9 @@ class compile2Csharp:
 ####################
 
 
-def openFile(fn):
+def openFile(projectdir, fn):
     try:
-        with open(f'{projectdir}/{fn}', "r") as f:
+        with open(f'{projectdir}/{fn}{fileType}', "r") as f:
             script = f.read()
             f.close()
             return script, None
@@ -4565,37 +4622,25 @@ def openFile(fn):
         return None, Error(e, PYTHON_EXCEPTION, Position(0, -1, -1, fn, ""), fn)
 
 
-def run(fn, text):
+def run(projectdir):
     # Reset data
-    global masterscript, metacode, outputdir, projectdir
+    global masterscript, metacode, usedLibs, outputdir
     masterscript = MasterScript([])
+    usedLibs = []
 
-    # Lexing
-    lexer = Lexer(fn, text)
-    tokens, error = lexer.genTokens()
+    # Parsing the project
+    error = ParseScripts(projectdir)
+    if error:
+        return error
+    error = ParseExternLibraries()
     if error:
         return error
 
-    # Parsing
-    parser = Parser(fn, tokens)
-    ast = parser.parse()
-    if ast.error:
-        return ast.error
-
-    # Parsing rest of the Project
-    error = getUsedScripts(fn)
-    if error:
-        return error
-
-    # Compiling
-    #compiler = compile2python(masterscript, outputdir, projectdir)
+    # Compiling the project
     compiler = compile2Csharp(masterscript, outputdir, projectdir)
     error = compiler.compile()
     if error:
         return error
-
-    # That's something for compile2python:
-    # compiler.checkPaths()
 
     return None
 
