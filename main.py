8d41ef7f0c4f0ff39d09b3249b992bf8d06b85b2
LePhobix_ <40542349+kobalt66@users.noreply.github.com>
2022-01-31 15:35:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import SimpleC
-from SimpleC import openFile, fileType
 outputData = None
 
 
@@ -7,41 +6,24 @@ def setOutputdir(dir):
     SimpleC.outputdir = dir
 
 
-def setProjectdir(dir):
-    SimpleC.projectdir = dir
-
 def retrunData():
     return outputData
 
 
-setProjectdir('Test Project/scripts')
-
-
 while True:
     script = ''
-    fn = ''
+    pn = ''
     text = input('[@] > ')
 
     if text.strip() == "":
         continue
-    if text.strip() == "run§":
-        fn = input('[FileName] > ')
-        if fn.strip() == "":
+    if text.strip() == "compile§":
+        pn = input('[Project directory] > ')
+        if pn.strip() == "":
             continue
 
-        script, error = openFile(fn + fileType)
-        if error:
-            error.throw()
-        error = SimpleC.run(f'{fn}', script)
-    else:
-        error = SimpleC.run("console", text)
+        error = SimpleC.run(pn)
 
     if error:
         error.throw()
         outputData = "Something went wrong! (No data received)"
-
-    # if result:
-    #     print(result)
-    #     print('\n\n\t => Everything worked!')
-    # elif error:
-    #     error.throw()
